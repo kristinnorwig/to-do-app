68c77b192f9f8ccdc6192855c05645bfec0ce969
Kristin Norwig <kristin.norwig@gmail.com>
2023-11-28 15:05:25 +0100

    feat: adds DELETE

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,25 +1,32 @@
 // globale Konstanten anlegen
 const list = document.querySelector("#todo-list"); // <ul>
-const currInput = document.querySelector("#new-todo");
-const description = currInput.value;
-const addBtn = document.querySelector("#add-btn");
-const filterButtons = document.querySelectorAll('input[type="radio"]'); // alle input types=radio ansprechen querySelectorAll('input[type="radio"]')
-const removeBtn = document.querySelector("#remove-btn");
-const all = document.querySelector("#all-checkbox");
-const open = document.querySelector("#open-checkbox");
-const done = document.querySelector("#done-checkbox");
+const currInput = document.querySelector("#new-todo"); // <input/> type=text
+const addBtn = document.querySelector("#add-btn"); // <button>
+const filterBtns = document.querySelectorAll('input[type="radio"]'); // alle input types=radio ansprechen querySelectorAll('input[type="radio"]')
+const removeBtn = document.querySelector("#remove-btn"); // <button>
+const all = document.querySelector("#all-checkbox"); // <input/> type=radio
+const open = document.querySelector("#open-checkbox"); // <input/> type=radio
+const done = document.querySelector("#done-checkbox"); // <input/> type=radio
 
-//Variable für Erstellung der ID, nach oben Zählen
-let idCounter = 0;
-
-// state anlegen
+// state anlegen (leeres Array)
 let state = [];
+
+// Funktion ausführen um API zu laden
 getTodosFromAPI();
 
-// todos aus api laden
+// todos aus api laden => später in addTodos() ausführen!!
 function getTodosFromAPI() {
+  //Netzwerkanfrage auf API URL
   fetch("http://localhost:4730/todos")
-    .then((request) => request.json())
+    // Verarbeitung der Antwort nach Anfrage, interpretation als JSON, enthält geparstes JSON
+    // Fragen ob fetch ok, wenn ja return, wenn nein Error
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not OK");
+      }
+    })
     // wenn geladen dann zeig die todos aus dem state (state sind geladene jsonData)
     .then((jsonData) => {
       // console.log(jsonData);
@@ -34,10 +41,16 @@ function addTodoToAPI(newTodo) {
     headers: {
       "content-type": "application/json",
     },
-    // neues todo als json in body übergeben
+    // neues todo in json-Format in body übergeben
     body: JSON.stringify(newTodo),
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not OK");
+      }
+    })
     .then((jsonData) => {
       getTodosFromAPI();
     });
@@ -59,32 +72,38 @@ function updateTodoToAPI(updatedTodo) {
     });
 }
 
-/*// lade LocalStorage
-if (localStorage.getItem("state")) {
-  state = JSON.parse(localStorage.getItem("state"));
-} else {
-  // wenn der LocalStorage leer ist, zeige folgendes an
-  state = {
-    todos: [
-      { description: "Einkaufen", done: true, id: 1 },
-      { description: "Putzen", done: false, id: 2 },
-    ],
-  };
-}*/
+function removeFromAPI(id) {
+  // id ist der Parameter, später wird Argument mitgegeben todoID in removeDoneTodos()
+  return fetch("http://localhost:4730/todos/" + id, {
+    // Fetch wird durch das RETURN nach außen gegeben für removeDoneTodos()
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not OK");
+      }
+    })
+    .then(() => {})
+    .catch((error) => {
+      alert(error.message);
+    });
+}
 
-// Todos aus Array mit HTML "verknüpfen" mit Funktion JS sichtbar machen
+// Todos aus Array mit HTML "verknüpfen", mit Funktion JS "sichtbar machen"
 function renderTodos() {
   list.innerHTML = "";
 
   for (const todo of state) {
-    const newTodoLi = document.createElement("li");
-    const checkbox = document.createElement("input");
+    let newTodoLi = document.createElement("li");
+    let checkbox = document.createElement("input");
     //Klasse vergeben
     checkbox.setAttribute = ("class", "todo-checkbox");
     // type festlegen
     checkbox.type = "checkbox";
 
-    // checked atrribute mit key done verbinden
+    // checked attribute mit key done verbinden
     checkbox.checked = todo.done;
 
     // Veränderung des Checkbox Status festhalten mit event auf Checkbox, Event-Listener für Änderungen am "done"-Status
@@ -97,9 +116,8 @@ function renderTodos() {
       todo.done = todoDoneState;
       console.log(todoDoneState);
 
-      // todo an API
+      // update todo an API, todo als Argument
       updateTodoToAPI(todo);
-      // localStorage.setItem("state", JSON.stringify(state)); // neue done Eigenschaft in localStorage speichern
     }
 
     const label = document.createElement("label");
@@ -121,15 +139,29 @@ renderTodos();
 
 function addTodo(event) {
   event.preventDefault(); // Wegen <form>
+  const description = currInput.value;
 
-  // Prüfe auf leere Eingabe im input-Feld
+  //Prüfe auf leere Eingabe im input-Feld
   if (description === "") {
     alert("Bitte Beschreibung eingeben!");
     return;
   }
 
+  // Prüfung auf doppelte Eingabe, ist Todo bereits vorhanden?
+  function isDescrDoubled(currInput) {
+    for (const todo of state) {
+      // alle einträge anschauen und mit input vergleichen case-insenstitive!!
+      if (todo.description.toLowerCase() === currInput.toLowerCase()) {
+        return true; // Duplikat gefunden
+      }
+    }
+    return false; // Kein Duplikat gefunden
+  }
+
+  const isDoubled = isDescrDoubled(description); // Konstante für Funktion mit description (=currInput.value) des todos als Parameter
+
   if (isDoubled) {
-    alert("Aufgabe existiert bereits!"); //error, warn => kein Pop-up
+    alert("Aufgabe existiert bereits!");
     return;
   }
 
@@ -137,12 +169,9 @@ function addTodo(event) {
   const newTodo = {
     description: description,
     done: false,
-    id: idGenerator(),
   };
 
   state.push(newTodo);
-
-  // localStorage.setItem("state", JSON.stringify(state));
 
   currInput.value = "";
 
@@ -152,26 +181,8 @@ function addTodo(event) {
   addTodoToAPI(newTodo);
 }
 
-// Prüfung auf doppelte Eingabe, ist Todo bereits vorhanden?
-function isDescrDoubled(currInput) {
-  for (const todo of state) {
-    // alle einträge anschauen und mit input vergleichen case-insenstitive!!
-    if (todo.description.toLowerCase() === currInput.toLowerCase()) {
-      return true; // Duplikat gefunden
-    }
-  }
-  return false; // Kein Duplikat gefunden
-}
-
-const isDoubled = isDescrDoubled(description); // Konstante für Funktion mit description (=currInput.value) des todos als Parameter
-
 // Funktion addTodo auf Button ausführen
 addBtn.addEventListener("click", addTodo);
-
-// Funktion um ID zu generieren
-function idGenerator() {
-  return idCounter++;
-}
 
 // remove Button  Event auslösen
 removeBtn.addEventListener("click", removeDoneTodos);
@@ -179,19 +190,33 @@ removeBtn.addEventListener("click", removeDoneTodos);
 // Remove done todos function
 function removeDoneTodos() {
   const openTodos = [];
+  const deleteTodos = [];
+  const fetches = [];
 
-  // alle Todos des states durchgehen
+  // alle Todos des states durchgehen und abfragen ob nicht abgehakt, dann in neues openTodos pushen, wenn abgehackt dann in deleteTodos
   for (const todo of state) {
     if (todo.done === false) {
       openTodos.push(todo);
+    } else {
+      deleteTodos.push(todo);
     }
   }
 
-  state = openTodos;
+  // alles in deleteTodos in array mit Funktion fetchen
+  for (const todo of deleteTodos) {
+    const todoID = todo.id;
+    fetches.push(removeFromAPI(todoID)); // todoID ist Argument
+  }
 
-  // localStorage.setItem("state", JSON.stringify(state));
-
-  renderTodos();
+  // Ansammlung von API Fetches in Arrays, wenn alle promises erfüllt wurden wird methode aufgerufen und array values gespeichert
+  Promise.all(fetches).then((values) => {
+    // wenn undefined nicht da ist (!= -1 bedeutet index -1 = gibt es nicht)
+    if (values.indexOf(undefined) != -1) {
+      console.log(values);
+      state = openTodos;
+      renderTodos();
+    }
+  });
 }
 
 //Filter function
@@ -208,14 +233,14 @@ function filterTodos() {
 
     // Wenn button open ist checked
   } else if (open.checked) {
-    // Zeige nur offene Todos
+    // Zeige nur offene Todos, die filter Methode filtert alle !todo.done also mit Wert false
     filteredTodos = state.filter(function (todo) {
       return !todo.done;
     });
 
     // Wenn button done ist checked
   } else if (done.checked) {
-    // Zeige nur erledigte Todos
+    // Zeige nur erledigte Todos, die filter Methode filtert alle todo.done also mit Wert true
     filteredTodos = state.filter(function (todo) {
       return todo.done;
     });
@@ -240,7 +265,7 @@ function showFilteredTodos(filteredTodos) {
     checkbox.addEventListener("change", function (event) {
       const todoDoneState = event.target.checked;
       todo.done = todoDoneState;
-      //localStorage.setItem("state", JSON.stringify(state));
+
       filterTodos(); // Filtern erneut aufrufen, um die Anzeige zu aktualisieren
     });
 
@@ -256,15 +281,15 @@ function showFilteredTodos(filteredTodos) {
   }
 }
 
-// schleife um alle radio buttons anzusehen und zu checken ob einer aktiv ist
-for (const oneButton of filterButtons) {
+// schleife um alle radio buttons anzusehen und zu checken ob einer aktiv ist, sodass nur ein Btn aktiv sein kann
+for (const oneBtn of filterBtns) {
   // Event-Listener für die Filter-Radio-Buttons
-  oneButton.addEventListener("change", function () {
+  oneBtn.addEventListener("change", function () {
     // Deaktiviere alle anderen Radio-Button
-    for (const otherButton of filterButtons) {
+    for (const otherBtn of filterBtns) {
       // wenn der andere aktive radio button nicht der geklickte radio button ist, dann stelle checked auf false
-      if (otherButton !== oneButton) {
-        otherButton.checked = false;
+      if (otherBtn !== oneBtn) {
+        otherBtn.checked = false;
       }
     }
 
